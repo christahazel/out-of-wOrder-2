405206abc1bf6d9ed53a42a917d79014c2db0a1e
christahazel <hazel.christa@gmail.com>
2017-05-03 22:45:38 -0400

    deleted

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import AppTwo from './AppTwo';
-// import AppThree from './AppThree';
-// import AppFour from './AppFour';
 import './index.css';
 
 ReactDOM.render(
